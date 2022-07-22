d57d7e818514535a53e11294b0d210333e1f33cc
phoenix <tony.cryptoken@gmail.com>
2022-07-22 23:23:21 +0800

    add close modal function

--- src/modal.rs
+++ src/modal.rs
@@ -45,9 +45,9 @@ impl Component for Modal {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let props = ctx.props();
-
+        let link = ctx.link();
         // self.is_open = props.is_open;
-
+        let close_modal = link.callback(|_| Msg::CloseModal());
         html! {
             <>
                 {
@@ -60,7 +60,7 @@ impl Component for Modal {
                                             <div class="header-title">
                                                 { "Select a token" }
                                             </div>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="close-modal"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                            <svg onclick={close_modal} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="close-modal"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                                         </div>
                                     </div>
 
