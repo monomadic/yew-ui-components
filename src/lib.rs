ae2c69a59e9bf4dc33e01a5df2e2aa26b65476ab
monomadic <monomadic.crypto>
2022-07-22 17:45:32 +0700

    remove clones on bool values

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ fn App() -> Html {
         Callback::from(move |_| {
             let is_open = is_open.clone();
             let show_modal_title = show_modal_title.clone();
-            if (*is_open).clone() {
+            if *is_open {
                 show_modal_title.set(String::from("Show Modal"));
                 is_open.set(false);
             } else {
@@ -55,7 +55,7 @@ fn App() -> Html {
                 <button {onclick}>{ (*show_modal_title).clone() }</button>
             </div>
 
-            <Modal is_open={(*is_open).clone()} />
+            <Modal is_open={*is_open} />
         </div>
     }
 }
