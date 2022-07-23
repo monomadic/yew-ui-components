ad09c86c79d15f66c2d92453e09c67645b62d5b3
phoenix <tony.cryptoken@gmail.com>
2022-07-23 12:50:14 +0800

    fix close modal

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -19,8 +19,11 @@ fn App() -> Html {
 
     let onselect: Callback<_> = {
         let selected_item = selected_item.clone();
+        let is_open = is_open.clone();
+
         Callback::from(move |item| {
             selected_item.set(item);
+            is_open.set(false);
         })
     };
 
@@ -29,13 +32,8 @@ fn App() -> Html {
         let is_open = is_open.clone();
         Callback::from(move |_| {
             let show_modal_title = show_modal_title.clone();
-            if *is_open {
-                show_modal_title.set(String::from("Show Modal"));
-                is_open.set(false);
-            } else {
-                show_modal_title.set(String::from("Hide Modal"));
-                is_open.set(true);
-            }
+            show_modal_title.set(String::from("Show Modal"));
+            is_open.set(true);
         })
     };
 
@@ -49,11 +47,11 @@ fn App() -> Html {
     //     })
     // };
 
-    // let header = html! {
-    //     <div>
-
-    //     </div>
-    // };
+    let modal_title = html! {
+        <div>
+            {"Select a token"}
+        </div>
+    };
 
     let body = html! {
         <div>
@@ -80,7 +78,7 @@ fn App() -> Html {
                 <button {onclick}>{ (*show_modal_title).clone() }</button>
             </div>
 
-            <Modal {is_open} {body} {footer} />
+            <Modal {is_open} {modal_title} {body} {footer} />
         </div>
     }
 }
--- src/modal.rs
+++ src/modal.rs
@@ -10,6 +10,8 @@ pub struct Props {
     #[prop_or(html!{<></>})]
     pub header: Html,
     #[prop_or(html!{<></>})]
+    pub modal_title: Html,
+    #[prop_or(html!{<></>})]
     pub body: Html,
     #[prop_or(html!{<></>})]
     pub footer: Html,
@@ -58,7 +60,7 @@ impl Component for Modal {
                                     <div class="header">
                                         <div class="row justify-content-between">
                                             <div class="header-title">
-                                                { "Select a token" }
+                                                { props.modal_title.clone() }
                                             </div>
                                             <svg onclick={close_modal} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="close-modal"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                                         </div>
