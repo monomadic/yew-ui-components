2c5f9015f6abad225d37eb5c96b8e0ba35ff63c3
phoenix <tony.cryptoken@gmail.com>
2022-07-28 23:57:11 +0800

    change the props default value

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,38 +1,37 @@
 use yew::prelude::*;
-use yew_ui_components::{Item, Modal, SelectItem};
+use yew_ui_components::Modal;
+// use yew_ui_components::{Item, Modal, SelectItem};
 
 #[function_component]
 fn App() -> Html {
-    let items = vec![
-        Item {
-            content: html! {"red"},
-            inner: String::from("red"),
-        },
-        Item {
-            content: html! {"blue"},
-            inner: String::from("blue"),
-        },
-    ];
+    // let items = vec![
+    //     Item {
+    //         content: html! {"red"},
+    //         inner: String::from("red"),
+    //     },
+    //     Item {
+    //         content: html! {"blue"},
+    //         inner: String::from("blue"),
+    //     },
+    // ];
     let selected_item = use_state(|| String::from("none"));
     let show_modal_title = use_state(|| String::from("Show Modal"));
     let is_open = use_state(|| false);
 
-    let onselect: Callback<_> = {
-        let selected_item = selected_item.clone();
-        let is_open = is_open.clone();
+    // let onselect: Callback<_> = {
+    //     let selected_item = selected_item.clone();
+    //     let is_open = is_open.clone();
 
-        Callback::from(move |item| {
-            selected_item.set(item);
-            is_open.set(false);
-        })
-    };
+    //     Callback::from(move |item| {
+    //         selected_item.set(item);
+    //         is_open.set(false);
+    //     })
+    // };
 
     let onclick = {
         let show_modal_title = show_modal_title.clone();
         let is_open = is_open.clone();
         Callback::from(move |_| {
-            let show_modal_title = show_modal_title.clone();
-            show_modal_title.set(String::from("Show Modal"));
             is_open.set(true);
         })
     };
@@ -49,17 +48,17 @@ fn App() -> Html {
 
     let modal_title = String::from("Modal title");
 
-    let body = html! {
-        <div>
-            <SelectItem<String> {onselect} {items} />
-        </div>
-    };
+    // let body = html! {
+    //     <div>
+    //         <SelectItem<String> {onselect} {items} />
+    //     </div>
+    // };
 
-    let footer = html! {
-        <div>
-            { "footer" }
-        </div>
-    };
+    // let footer = html! {
+    //     <div>
+    //         { "footer" }
+    //     </div>
+    // };
 
     let is_open: bool = *is_open.clone();
     let modal_id: String = String::from("Modal_1");
@@ -72,7 +71,7 @@ fn App() -> Html {
                 <button {onclick}>{ (*show_modal_title).clone() }</button>
             </div>
 
-            <Modal {is_open} {modal_title} {body} {footer} id={modal_id} />
+            <Modal {is_open} {modal_title} id={modal_id} />
         </div>
     }
 }
--- src/modal.rs
+++ src/modal.rs
@@ -1,4 +1,4 @@
-use yew::{html, Component, Context, Html, Properties};
+use yew::{classes, html, Component, Context, Html, Properties};
 
 pub enum Msg {
     CloseModal(),
@@ -6,28 +6,36 @@ pub enum Msg {
 
 #[derive(Default, PartialEq, Properties)]
 pub struct Props {
-	#[prop_or(String::from(""))]
-	pub id: String,
-	#[prop_or(String::from(""))]
-	pub class_name: String,
-
-	pub is_open: bool,
-	#[prop_or(String::from(""))]
-	pub modal_class: String,
-	#[prop_or(String::from(""))]
-	pub modal_title: String,
+    #[prop_or(String::from(""))]
+    pub id: String,
+    #[prop_or(String::from(""))]
+    pub class_name: String,
+    #[prop_or(false)]
+    pub is_open: bool,
+    #[prop_or(String::from(""))]
+    pub modal_class: String,
+    #[prop_or(String::from(""))]
+    pub modal_title: String,
 
     #[prop_or(html!{<></>})]
     pub header: Html,
     #[prop_or(String::from(""))]
     pub header_class: String,
 
-    #[prop_or(html!{<></>})]
+    #[prop_or(html!{
+        <div class={ "yew-modal-body " }>
+            { "Modal Body" }
+        </div>
+    })]
     pub body: Html,
     #[prop_or(String::from(""))]
     pub body_class: String,
 
-    #[prop_or(html!{<></>})]
+    #[prop_or(html!{
+        <div class={ "yew-modal-footer " }>
+            { "Modal footer" }
+        </div>
+    })]
     pub footer: Html,
     #[prop_or(String::from(""))]
     pub footer_class: String,
@@ -47,7 +55,7 @@ impl Component for Modal {
         }
     }
 
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::CloseModal() => {
                 self.is_open = false;
@@ -56,47 +64,45 @@ impl Component for Modal {
         true
     }
 
-    fn changed(&mut self, _ctx: &Context<Self>) -> bool {
-        self.is_open = _ctx.props().is_open;
+    fn changed(&mut self, ctx: &Context<Self>) -> bool {
+        self.is_open = ctx.props().is_open;
         true
     }
 
-	fn view(&self, ctx: &Context<Self>) -> Html {
-		let props = ctx.props();
-		let link = ctx.link();
-		// self.is_open = props.is_open;
-		let close_modal = link.callback(|_| Msg::CloseModal());
-		html! {
-			<>
-				<div id={ props.id.clone() } class={if self.is_open { props.class_name.clone() + "yew-modal" } else { props.class_name.clone() + "yew-modal display-none" }}>
-					<div class="yew-modal-content">
-						<div class={ "yew-modal-header ".to_string() + &props.header_class }>
-							{
-								if props.modal_title.clone() != String::from("") {
-									html! {
-										<div class="row justify-content-between">
-											<div class="yew-modal-header-title">
-												{ props.modal_title.clone() }
-											</div>
-											<svg onclick={close_modal} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="yew-modal-close-button"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-										</div>
-									}
-								} else {
-									props.header.clone()
-								}
-							}
-						</div>
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let props = ctx.props();
+        let link = ctx.link();
+        // self.is_open = props.is_open;
 
-						<div class={ "yew-modal-body ".to_string() + &props.body_class }>
-							{ props.body.clone() }
-						</div>
+        let close_modal = link.callback(|_| Msg::CloseModal());
 
-						<div class={ "yew-modal-footer ".to_string() + &props.footer_class }>
-							{ props.footer.clone() }
-						</div>
-					</div>
-				</div>
-			</>
-		}
-	}
+        html! {
+            <>
+                <div id={ props.id.clone() } class={classes!("yew-modal", props.class_name.clone(), if self.is_open { "" } else { "display-none" })}>
+                    <div class="yew-modal-content">
+                        <div class={ "yew-modal-header ".to_string() + &props.header_class }>
+                            {
+                                if props.modal_title.clone() != String::from("") {
+                                    html! {
+                                        <div class="row justify-content-between">
+                                            <div class="yew-modal-header-title">
+                                                { props.modal_title.clone() }
+                                            </div>
+                                            <svg onclick={close_modal} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="yew-modal-close-button"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                        </div>
+                                    }
+                                } else {
+                                    props.header.clone()
+                                }
+                            }
+                        </div>
+
+                        { props.body.clone() }
+
+                        { props.footer.clone() }
+                    </div>
+                </div>
+            </>
+        }
+    }
 }
